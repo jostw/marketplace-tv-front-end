cfb7235b8f19129d4ce3ef721d66404a66d78687
Joseph Yeh <jyeh@mozilla.com>
2015-11-18 16:48:27 +0800

    Feature - Add wrap-around to spatial navigation

--- src/media/js/views/homepage.js
+++ src/media/js/views/homepage.js
@@ -2,7 +2,12 @@ define('views/homepage',
     ['core/l10n', 'core/models', 'core/z', 'templates', 'spatial-navigation'],
     function(l10n, models, z, nunjucks, SpatialNavigation) {
     var gettext = l10n.gettext;
+
     var appsModel = models('apps');
+    var appsLength = 0;
+
+    // Number of apps in a row.
+    var COUNT = 7;
 
     // Initialize spatial navigation.
     SpatialNavigation.init();
@@ -13,6 +18,8 @@ define('views/homepage',
     });
 
     z.page.on('loaded', function() {
+        appsLength = document.getElementsByClassName('focusable').length;
+
         // Add 'tabindex="-1"' to "currently-existing" navigable elements.
         SpatialNavigation.makeFocusable();
 
@@ -27,6 +34,35 @@ define('views/homepage',
                 app: appsModel.lookup($(this).data('slug'))
             })
         );
+    });
+
+    z.page.on('sn:navigatefailed', '.focusable', function(e) {
+        var dir = e.originalEvent.detail.direction;
+
+        var appIndex = parseInt(this.id.match(/focusable-(.+)/)[1]);
+        var focusIndex;
+
+        if (dir === 'up') {
+            // Last row is not full.
+            if (appIndex + Math.floor(appsLength / COUNT) * COUNT < appsLength) {
+                focusIndex = appIndex + Math.floor(appsLength / COUNT) * COUNT;
+            } else {
+                focusIndex = appIndex + (Math.floor(appsLength / COUNT) - 1) * COUNT;
+            }
+        } else if (dir === 'right') {
+            focusIndex = appIndex - COUNT + 1;
+        } else if (dir === 'down') {
+            focusIndex = appIndex % COUNT;
+        } else if (dir === 'left') {
+            // Last row is not full.
+            if (appIndex + COUNT > appsLength - 1) {
+                focusIndex = appsLength - 1;
+            } else {
+                focusIndex = appIndex + COUNT - 1;
+            }
+        }
+
+        SpatialNavigation.focus(document.getElementById('focusable-' + focusIndex));
     });
 
     return function(builder) {
