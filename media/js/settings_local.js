0d2d61fb26fbe1b737bb26f3190719fc3e93c54a
Joseph Yeh <jyeh@mozilla.com>
2015-11-19 16:28:37 +0800

    Deploy to GitHub Pages

--- media/js/settings_local.js
+++ media/js/settings_local.js
@@ -5,6 +5,6 @@ define('settings_local',
         // Remove to have data fetched from CDN rather than API.
         api_cdn_whitelist: {},
         api_url: 'https://marketplace.firefox.com',
-        media_url: 'https://marketplace-dev-cdn.allizom.org/media/'
+        media_url: 'https://marketplace.cdn.mozilla.net/media/',
     };
 });
